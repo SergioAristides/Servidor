30c3efdd3aadf5f288824de3a5dac9ed731a8ca9
SergioAristides <sergioa.riverab@autonoma.edu.co>
2022-11-25 18:16:29 -0500

    primer commit

---
+++ CineProyect/src/main/java/com/Cine/CineProyect/CineProyectApplication.java
@@ -0,0 +1,13 @@
+package com.Cine.CineProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineProyectApplication.class, args);
+	}
+
+}
